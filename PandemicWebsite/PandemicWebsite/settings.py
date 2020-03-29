2dd42dbf0441f90a5443cddb9d8bf3a62a7aabb3
Dhyey Lalseta <dhyeylalseta@outlook.com>
2020-03-28 18:41:02 -0600

    HTML routing
    -renders to index.html rather than one line https response

--- PandemicWebsite/PandemicWebsite/settings.py
+++ PandemicWebsite/PandemicWebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'landing.apps.LandingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'PandemicWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\dhyey\Projects\PandemicResponse\PandemicWebsite\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- PandemicWebsite/landing/views.py
+++ PandemicWebsite/landing/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+results = [
+    {
+        'address': 'Hospital Drive, Calgary',
+        'patient_capacity': '250',
+        'wait_time': '20',
+        'drive_time': '10',
+    },
+    {
+        'address': '2nd Hospital Drive, Calgary',
+        'patient_capacity': '69',
+        'wait_time': '30',
+        'drive_time': '20',
+    }
+]
+
 
 def home(request):
     """
     Returns HTTP Response for the home/landing page.
     """
-    return HttpResponse('<h1> You are at Landing</h1>')
+    context = {
+        'results': results
+    }
+    return render(request, 'landing/index.html', context)
